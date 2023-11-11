6c2ec874e556f8cc1870fad635f6477a836ed316
emreyh <emre.reyhanlioglu@gmail.com>
2023-11-11 13:38:18 +0300

    Refactor version increment logic and update icon.

--- src/main/java/com/emreyh/projectversionincrement/action/IncrementVersionAction.java
+++ src/main/java/com/emreyh/projectversionincrement/action/IncrementVersionAction.java
@@ -21,201 +21,182 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Optional;
 
 public class IncrementVersionAction extends AnAction {
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    if (project == null) {
-      return;
-    }
-    updateVersionNumber(project);
-  }
-
-  private void updateVersionNumber(Project project) {
-    String chartFilePath = project.getBasePath() + "/helm/Chart.yaml";
-    File chartFile = new File(chartFilePath);
-    if (!chartFile.exists()) {
-      System.out.println("Chart.yaml file does not exist");
-      return;
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+        updateVersionNumber(project);
     }
 
-    try {
-      // Read the contents of the Chart.yaml file
-      String chartFileContents = new String(Files.readAllBytes(chartFile.toPath()));
+    private void updateVersionNumber(Project project) {
+        try {
+            Optional<String> newVersionNumberOpt = updatePomVersion(project);
 
-      // Extract current version from Chart.yaml
-      String currentVersion = extractVersionFromChartYaml(chartFileContents);
-      if (currentVersion == null) {
-        System.out.println("Invalid version format in Chart.yaml");
-        return;
-      }
-
-      // Increment version number
-      String newVersion = incrementVersion(currentVersion);
-
-      // Update Chart.yaml
-      chartFileContents = updateVersionInChartYaml(chartFileContents, newVersion);
-      Files.write(chartFile.toPath(), chartFileContents.getBytes());
-
-      // Update pom.xml
-      updatePomVersion(project, newVersion);
-
-      // Refresh the files in IntelliJ IDEA
-      refreshFile(chartFile);
-      refreshFile(getPomFile(project));
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private String extractVersionFromChartYaml(String chartFileContents) {
-    String versionTag = "version:";
-    int versionTagIndex = chartFileContents.indexOf(versionTag);
-    if (versionTagIndex != -1) {
-      int versionStartIndex = versionTagIndex + versionTag.length();
-      int versionEndIndex = chartFileContents.indexOf('\n', versionStartIndex);
-      if (versionEndIndex != -1) {
-        return chartFileContents.substring(versionStartIndex, versionEndIndex).trim();
-      }
-    }
-    return null;
-  }
-
-  private String incrementVersion(String currentVersion) {
-    String[] versionArray = currentVersion.split("\\.");
-    if (versionArray.length == 3) {
-      int majorVersion = Integer.parseInt(versionArray[0]);
-      int minorVersion = Integer.parseInt(versionArray[1]);
-      int patchVersion = Integer.parseInt(versionArray[2]);
-
-      if (patchVersion < 100) {
-        versionArray[2] = String.valueOf(++patchVersion);
-      } else if (minorVersion < 100) {
-        versionArray[1] = String.valueOf(++minorVersion);
-        versionArray[2] = "0";
-      } else {
-        versionArray[0] = String.valueOf(++majorVersion);
-        versionArray[1] = "0";
-        versionArray[2] = "0";
-      }
-
-      return String.join(".", versionArray);
-    }
-    return currentVersion;
-  }
-
-  private String updateVersionInChartYaml(String chartFileContents, String newVersion) {
-    String versionTag = "version:";
-    String appVersionTag = "appVersion:";
-
-    // Replace the version field with the new version number
-    chartFileContents =
-        chartFileContents.replaceAll("(?m)^" + versionTag + ".*$", versionTag + " " + newVersion);
-
-    // Replace the appVersion field with the new version number
-    chartFileContents = chartFileContents.replaceAll("(?m)^" + appVersionTag + ".*$",
-        appVersionTag + " " + newVersion);
-
-    return chartFileContents;
-  }
-
-
-  private void updatePomVersion(Project project, String newVersion) {
-    File pomFile = getPomFile(project);
-    if (pomFile == null || !pomFile.exists()) {
-      System.out.println("pom.xml file does not exist");
-      return;
+            if (newVersionNumberOpt.isPresent()) {
+                updateVersionInChartYaml(project.getBasePath(), newVersionNumberOpt.get());
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    try {
-      Document document = getDocument(pomFile);
-      Node versionNode = getVersionNode(document);
-      if (versionNode == null) {
-        System.out.println("Version node not found in pom.xml");
-        return;
-      }
+    private String incrementVersion(String currentVersion) {
+        String[] versionArray = currentVersion.split("\\.");
+        if (versionArray.length == 3) {
+            int majorVersion = Integer.parseInt(versionArray[0]);
+            int minorVersion = Integer.parseInt(versionArray[1]);
+            int patchVersion = Integer.parseInt(versionArray[2]);
 
-      String versionProperty = versionNode.getTextContent();
-      if (versionProperty.startsWith("${") && versionProperty.endsWith("}")) {
-        String versionPropertyName = versionProperty.substring(2, versionProperty.length() - 1);
-        Node versionPropertyNode = getVersionPropertyNode(document, versionPropertyName);
-        versionPropertyNode.setTextContent(newVersion);
-      } else {
-        versionNode.setTextContent(newVersion);
-      }
+            if (patchVersion < 100) {
+                versionArray[2] = String.valueOf(++patchVersion);
+            } else if (minorVersion < 100) {
+                versionArray[1] = String.valueOf(++minorVersion);
+                versionArray[2] = "0";
+            } else {
+                versionArray[0] = String.valueOf(++majorVersion);
+                versionArray[1] = "0";
+                versionArray[2] = "0";
+            }
 
-      writePom(pomFile, document);
-      reformatPom(project, pomFile);
-    } catch (Exception e) {
-      e.printStackTrace();
+            return String.join(".", versionArray);
+        }
+        return currentVersion;
     }
-  }
 
-  private File getPomFile(Project project) {
-    String projectPath = project.getBasePath();
-    if (projectPath != null) {
-      return new File(projectPath + "/pom.xml");
+    private void updateVersionInChartYaml(String basePath, String newVersion) throws IOException {
+        File chartFile = getChartFile(basePath);
+        if (!chartFile.exists()) {
+            System.out.println("Chart.yaml file not found. Skipped version increment operation.");
+            return;
+        }
+
+        String chartFileContents = new String(Files.readAllBytes(chartFile.toPath()));
+
+        String versionTag = "version:";
+        String appVersionTag = "appVersion:";
+
+        // Replace the version field with the new version number
+        chartFileContents =
+                chartFileContents.replaceAll("(?m)^" + versionTag + ".*$", versionTag + " " + newVersion);
+
+        // Replace the appVersion field with the new version number
+        chartFileContents = chartFileContents.replaceAll("(?m)^" + appVersionTag + ".*$",
+                appVersionTag + " " + newVersion);
+
+        Files.write(chartFile.toPath(), chartFileContents.getBytes());
+
+        refreshFile(chartFile);
     }
-    return null;
-  }
 
-  private void writePom(File pomFile, Document document) throws TransformerException {
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer transformer = transformerFactory.newTransformer();
-    DOMSource source = new DOMSource(document);
-    StreamResult result = new StreamResult(pomFile);
-    transformer.transform(source, result);
-  }
 
-  private Node getVersionNode(Document document) throws XPathExpressionException {
-    XPathFactory xpathFactory = XPathFactory.newInstance();
-    XPath xpath = xpathFactory.newXPath();
-    XPathExpression expr = xpath.compile("/project/version");
-    return (Node) expr.evaluate(document, XPathConstants.NODE);
-  }
+    private Optional<String> updatePomVersion(Project project) {
+        File pomFile = getPomFile(project.getBasePath());
+        if (!pomFile.exists()) {
+            System.out.println("pom.xml file does not exist");
+            return Optional.empty();
+        }
 
-  private Node getVersionPropertyNode(Document document, String propertyName) {
-    try {
-      XPathFactory xpathFactory = XPathFactory.newInstance();
-      XPath xpath = xpathFactory.newXPath();
-      XPathExpression expr = xpath.compile("/project/properties/" + propertyName);
-      return (Node) expr.evaluate(document, XPathConstants.NODE);
-    } catch (Exception e) {
-      e.printStackTrace();
+        try {
+            Document document = getDocument(pomFile);
+            Optional<Node> versionNode = getVersionNode(document);
+            if (versionNode.isEmpty()) {
+                System.out.println("Version node not found in pom.xml");
+                return Optional.empty();
+            }
+
+            String versionProperty = versionNode.get().getTextContent();
+            if (versionProperty.startsWith("${") && versionProperty.endsWith("}")) {
+                String versionPropertyName = versionProperty.substring(2, versionProperty.length() - 1);
+                versionNode = getVersionPropertyNode(document, versionPropertyName);
+            }
+
+            if (versionNode.isEmpty()) {
+                System.err.println("The version node can not be empty. Skipped version increment operation.");
+                return Optional.empty();
+            }
+
+            Node pomVersionNode = versionNode.get();
+            String pomVersion = pomVersionNode.getTextContent();
+            String incrementedPomVersion = incrementVersion(pomVersion);
+
+            pomVersionNode.setTextContent(incrementedPomVersion);
+
+            writePom(pomFile, document);
+            reformatPom(project, pomFile);
+            refreshFile(pomFile);
+
+            return Optional.of(incrementedPomVersion);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return Optional.empty();
+        }
     }
-    return null;
-  }
 
-  private Document getDocument(File pomFile)
-      throws ParserConfigurationException, SAXException, IOException {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder builder = factory.newDocumentBuilder();
-    return builder.parse(pomFile);
-  }
-
-  private void reformatPom(Project project, File pomFile) {
-    VirtualFile virtualFile = VcsUtil.getVirtualFile(pomFile);
-    if (virtualFile != null) {
-      VcsDirtyScopeManager.getInstance(project).dirDirtyRecursively(virtualFile);
-      refreshFile(pomFile);
+    private File getChartFile(String basePath) {
+        return new File(basePath + "/helm/Chart.yaml");
     }
-  }
 
-  private void refreshFile(File file) {
-    VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file);
-    if (virtualFile != null) {
-      VfsUtil.markDirtyAndRefresh(false, true, true, virtualFile);
+    private File getPomFile(String basePath) {
+        return new File(basePath + "/pom.xml");
     }
-  }
+
+    private void writePom(File pomFile, Document document) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(pomFile);
+        transformer.transform(source, result);
+    }
+
+    private Optional<Node> getVersionNode(Document document) throws XPathExpressionException {
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+        XPathExpression expr = xpath.compile("/project/version");
+        return Optional.ofNullable((Node) expr.evaluate(document, XPathConstants.NODE));
+    }
+
+    private Optional<Node> getVersionPropertyNode(Document document, String propertyName) {
+        try {
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            XPath xpath = xpathFactory.newXPath();
+            XPathExpression expr = xpath.compile("/project/properties/" + propertyName);
+            return Optional.ofNullable((Node) expr.evaluate(document, XPathConstants.NODE));
+        } catch (Exception e) {
+            System.err.println("Version node not found.");
+        }
+
+        return Optional.empty();
+    }
+
+    private Document getDocument(File pomFile)
+            throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(pomFile);
+    }
+
+    private void reformatPom(Project project, File pomFile) {
+        VirtualFile virtualFile = VcsUtil.getVirtualFile(pomFile);
+        if (virtualFile != null) {
+            VcsDirtyScopeManager.getInstance(project).dirDirtyRecursively(virtualFile);
+            refreshFile(pomFile);
+        }
+    }
+
+    private void refreshFile(File file) {
+        VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file);
+        if (virtualFile != null) {
+            VfsUtil.markDirtyAndRefresh(false, true, true, virtualFile);
+        }
+    }
 }
